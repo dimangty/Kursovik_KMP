1239d39e469755655feb4a4579875950763ce6b4
dmitriy.bykov <dmitriy.bykov@elementpay.io>
2025-02-24 11:15:23 +0700

    Add ios delegate

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,26 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool  {
+        let mNetworkService: NewsService = inject()
+        do {
+            let model: NewsListViewModel = try inject()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        return true
+    }
 }
