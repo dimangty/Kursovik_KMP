1e684637e8bd47bebbf66f644719ad6ed97eb68a
dmitriy.bykov <dmitriy.bykov@elementpay.io>
2025-02-26 16:10:53 +0700

    Add Compose Screens

---
+++ androidApp/src/main/java/com/example/kursovikkmp/android/Screens.kt
@@ -0,0 +1,7 @@
+package com.example.kursovikkmp.android
+
+sealed class Screens(val route : String) {
+    object Home : Screens("home_screen")
+    object Search : Screens("search_screen")
+    object Profile : Screens("profile_screen")
+}
